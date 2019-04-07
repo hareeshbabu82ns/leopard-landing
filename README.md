221c5ec9bd9284713ab85079a25bdfd109e109ba
Hareesh Polla <hpolla@atb.com>
2019-04-07 07:20:10 -0600

    SendGrid lamda

--- README.md
+++ README.md
@@ -7,7 +7,13 @@
   $> npm run build
 ```
 
-## Deploy to Zeit (now)
+## Deploy to Zeit [now](https://leopard.terabits.now.sh)
 ```sh
-  $> npm run now
+  $> now
 ```
+
+## Add Secrets
+```sh
+  $> now secret add sg-api-key <api_key>
+```
+
--- api/index.js
+++ api/index.js
@@ -1,3 +1,20 @@
+const sgMail = require('@sendgrid/mail');
+
 module.exports = (req, res) => {
-  res.end(`Hello from Node.js on Now 2.0!`);
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: 'mail@gmail.com',
+    from: 'mail1@gmail.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail.send(msg, false, (err, result) => {
+    if (err) {
+      console.log('mail sending failed', err);
+      throw err;
+    }
+    console.log('mail successfully sent', result);
+    res.end('mail sent successfully');
+  });
 };
